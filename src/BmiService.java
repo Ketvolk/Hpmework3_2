bd7de61553ad6b536306f46dae6d1209798b498e
Ekaterina <ketvolk@mail.ru>
2023-04-29 12:30:37 +0300

    CodeFormat changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate (int mKg, double hMeter) {
-        int result = (int) (mKg/(hMeter*hMeter));
+    public int calculate(int mKg, double hMeter) {
+        int result = (int) (mKg / (hMeter * hMeter));
         return result;
     }
 }
